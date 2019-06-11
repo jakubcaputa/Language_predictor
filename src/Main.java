75e1ae8d13b0f31f50d926d1794a77872386ad2c
patrykokruta <patrykokruta98@gmail.com>
2019-06-11 18:40:41 +0200

    added program menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,74 @@
-public class Main {
+import java.util.Scanner;
 
+// Main executive class
+// User provides text
+// The app recognizes language and allows to predict next words based on previous
+public class Main {
+    private Main() {
+    };
+
+    public static void main(String[] args) throws Exception {
+
+        boolean program_loop = true;
+
+        while (program_loop) {
+
+            System.out.println("1. Train algorithm");
+            System.out.println("2. Recognize language using unigram");
+            System.out.println("3. Recognize language using bigram");
+            System.out.println("4. Word prediction using bigram");
+            System.out.println("5. Exit");
+
+            String url;
+            Scanner scan = new Scanner(System.in);
+            switch (scan.nextInt()){
+                case 1:
+                    System.out.println("Set a language you want to train:");
+                    System.out.println("1. Polish");
+                    System.out.println("2. English");
+                    System.out.println("3. German");
+                    scan = new Scanner(System.in);
+                    switch(scan.nextInt()){
+                        case 1:
+                            System.out.println("Waiting for URL:");
+                            scan = new Scanner(System.in);
+                            url = scan.nextLine();
+
+                            break;
+                        case 2:
+                            System.out.println("Waiting for URL:");
+                            scan = new Scanner(System.in);
+                            url = scan.nextLine();
+                            break;
+                        case 3:
+                            System.out.println("Waiting for URL:");
+                            scan = new Scanner(System.in);
+                            url = scan.nextLine();
+                            break;
+                    }
+                case 2:
+
+                    System.out.println("Waiting for URL:");
+                    scan = new Scanner(System.in);
+                    url = scan.nextLine();
+                    break;
+                case 3:
+
+                    System.out.println("Waiting for URL:");
+                    scan = new Scanner(System.in);
+                    url = scan.nextLine();
+                    break;
+
+                case 4:
+
+                    System.out.println("Waiting for a word:");
+                    break;
+
+                case 5:
+                    program_loop = false;
+            }
+
+            break;
+        }
+    }
 }
